de4a8f8cc4e06b612e1c79b2e7a3d12496921e30
Cezar H <29507335+usernein@users.noreply.github.com>
2020-06-13 20:56:44 -0300

    try to fix infinite run

--- .github/workflows/android.py
+++ .github/workflows/android.py
@@ -3,7 +3,7 @@ import pyrogram
 
 caption = "oi hitalo"
 
-with pyrogram.Client(':memory:', os.getenv('API_ID'), os.getenv('API_HASH'), bot_token=os.getenv('BOT_TOKEN')) as client:
+with pyrogram.Client('bot', os.getenv('API_ID'), os.getenv('API_HASH'), bot_token=os.getenv('BOT_TOKEN')) as client:
     client.send_document(
         chat_id=os.getenv('CHAT_ID'),
         document=os.getenv('FILENAME'),
