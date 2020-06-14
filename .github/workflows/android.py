ecc77d0cbe8665fb28e69673a0c2cd3fca985fe7
Cezar H <29507335+usernein@users.noreply.github.com>
2020-06-14 15:39:36 -0300

    Recursively send all apks under source/app/build/outputs/apk

--- .github/workflows/android.py
+++ .github/workflows/android.py
@@ -1,11 +1,14 @@
 import os
 import pyrogram
 
+from glob import glob
+
 caption = "que nem o do Alisson Cezar"
 
 with pyrogram.Client('bot', os.getenv('API_ID'), os.getenv('API_HASH'), bot_token=os.getenv('BOT_TOKEN')) as client:
-    client.send_document(
-        chat_id=os.getenv('CHAT_ID'),
-        document=os.getenv('FILENAME'),
-        caption=caption
-    )
+    for path in glob('source/app/build/outputs/apk/**/*.apk', recursive=True):
+        client.send_document(
+            chat_id=os.getenv('CHAT_ID'),
+            document=path,
+            caption=path
+        )
