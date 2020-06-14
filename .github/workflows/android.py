d7d54c97fb440b1688cc31a12825e108c7c03a44
Cezar H <29507335+usernein@users.noreply.github.com>
2020-06-14 09:31:56 -0300

    Change directory name and change caption to what o hitalo pediu

--- .github/workflows/android.py
+++ .github/workflows/android.py
@@ -1,7 +1,7 @@
 import os
 import pyrogram
 
-caption = "oi hitalo"
+caption = "que nem o do Alisson Cezar"
 
 with pyrogram.Client('bot', os.getenv('API_ID'), os.getenv('API_HASH'), bot_token=os.getenv('BOT_TOKEN')) as client:
     client.send_document(
