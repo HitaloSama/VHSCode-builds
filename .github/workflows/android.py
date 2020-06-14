2297a07e40c20700a2a82f3e1ea16329c65944c5
Cezar H <29507335+usernein@users.noreply.github.com>
2020-06-14 15:49:00 -0300

    fix -r to -R and move glob start to cwd

--- .github/workflows/android.py
+++ .github/workflows/android.py
@@ -6,7 +6,7 @@ from glob import glob
 caption = "que nem o do Alisson Cezar"
 
 with pyrogram.Client('bot', os.getenv('API_ID'), os.getenv('API_HASH'), bot_token=os.getenv('BOT_TOKEN')) as client:
-    for path in glob('source/app/build/outputs/apk/**/*.apk', recursive=True):
+    for path in glob('**/*.apk', recursive=True):
         client.send_document(
             chat_id=os.getenv('CHAT_ID'),
             document=path,
