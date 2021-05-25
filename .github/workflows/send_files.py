45d55dd30398f249afce783d1628cfe00284ae33
Hitalo <hitalo331@outlook.com>
2021-05-25 10:21:05 -0300

    General improvements

--- .github/workflows/send_files.py
+++ .github/workflows/send_files.py
@@ -1,18 +1,20 @@
 import os
-import pyrogram
-
 from glob import glob
 
-short_rev=os.getenv('SHORT_REV')
-long_rev=os.getenv('LONG_REV')
-branch=os.getenv('BRANCH')
-time=os.getenv('TIME')
+import pyrogram
 
-with pyrogram.Client('bot', os.getenv('API_ID'), os.getenv('API_HASH'), bot_token=os.getenv('BOT_TOKEN')) as client:
-    for path in glob('**/*.apk', recursive=True):
+short_rev = os.getenv("SHORT_REV")
+long_rev = os.getenv("LONG_REV")
+branch = os.getenv("BRANCH")
+time = os.getenv("TIME")
+
+with pyrogram.Client(
+    "bot", os.getenv("API_ID"), os.getenv("API_HASH"), bot_token=os.getenv("BOT_TOKEN")
+) as client:
+    for path in glob("**/*.apk", recursive=True):
         client.send_document(
             document=path,
             caption=f"""APK Built with the [{short_rev}](https://github.com/vhqtvn/VHEditor-Android/commit/{long_rev}) commit (Commit made on {time}).""",
-        chat_id=os.getenv('CHAT_ID'),
-        parse_mode="markdown"
+            chat_id=os.getenv("CHAT_ID"),
+            parse_mode="markdown",
         )
--- README.md
+++ README.md
@@ -1,7 +1,11 @@
-# VHS Code builds
+# VHEditor-Android CI
 
 ![](https://github.com/Project-WKF/VHSCode-builds/workflows/Build/badge.svg)
 
-This repo if for build VHEditor APKs from https://github.com/vhqtvn/VHEditor-Android
+> This repo if for build VHEditor APKs from https://github.com/vhqtvn/VHEditor-Android
 
-You can get builds at https://t.me/VHEditor
+## Downloads 
+
+Builds made through this CI are sent to Telegram.
+
+- [@VHSCodeBuilds](https://t.me/VHSCodeBuilds )
